520ae6a86179e544e2d3950f524e903bb7dffcc7
mikachou <chou1005.tw@gmail.com>
2023-07-17 20:51:28 +0800

    add Main.css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Main.css";
 import StepProgress from "./Step/StepProgress/StepProgress";
 import FormPayment from "./Form/FormPayment";
 import Buttons from "./ControlButton/Buttons";
 
 function Main() {
   return (
-    <div>
+    <div className="main">
       <div className="leftZone">
         <StepProgress />
         <FormPayment />
