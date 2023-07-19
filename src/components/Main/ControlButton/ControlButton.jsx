0302201858360e133d81824413752ea8a7626f91
mikachou <chou1005.tw@gmail.com>
2023-07-19 14:51:24 +0800

    delete import React from components

--- src/components/Main/ControlButton/ControlButton.jsx
+++ src/components/Main/ControlButton/ControlButton.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./ControlButton.css";
 
 function ControlButton() {
--- src/components/Main/Step/StepProgress/StepProgress.jsx
+++ src/components/Main/Step/StepProgress/StepProgress.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../StepProgress/StepProgress.css";
 
 function StepProgress() {
