2594afe8b5c43fab7e70a1019e81cf021c0ddd0a
mikachou <chou1005.tw@gmail.com>
2023-07-19 14:56:29 +0800

    add Footer and Header .jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../src/App.css";
+import Header from "./components/Header/Header";
 import Main from "../src/components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import "./Header.css";
+
+function Header() {
+  return;
+  <div className="header"></div>;
+}
+
+export default Header;
