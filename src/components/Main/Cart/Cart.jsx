1e79037a948d8738af9f0152355bfbc14da6857b
mikachou <chou1005.tw@gmail.com>
2023-07-20 12:50:50 +0800

    render data in Cart.jsx

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -18,36 +18,34 @@ const products = [
 ];
 
 function Cart() {
+  const listItems = products.map((product) => (
+    <div className="item">
+      <img className="itemPic" src={product.img} alt={product.name} />
+      <div className="itemRright">
+        <div className="itemInfo">
+          <h5>{product.name}</h5>
+          <h5>${product.price}</h5>
+        </div>
+        <div className="addItem">
+          <img src="/icons/minus.svg" alt="" />
+          <p>{product.quantity}</p>
+          <img src="/icons/plus.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
     <div className="cart">
       <div className="cartWrapper">
         <h3>購物籃</h3>
-        <div className="cartItems">
-          <div className="item">
-            <img
-              className="itemPic"
-              src="https://picsum.photos/300/300?text=1"
-              alt="itemPic"
-            />
-            <div className="itemRright">
-              <div className="itemInfo">
-                <h5>Product Name</h5>
-                <h5>$3,000</h5>
-              </div>
-              <div className="addItem">
-                <img src="/icons/minus.svg" alt="" />
-                <p>1</p>
-                <img src="/icons/plus.svg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr />
+        <div className="cartItems">{listItems}</div>
+
         <div className="shipFee">
           <h5>運費</h5>
           <h5>免費</h5>
         </div>
-        <hr />
+
         <div className="totalPrice">
           <h5>小計</h5>
           <h5>$100,000</h5>
