67727d69e6bda4f64c36af14431c546c41c2e2a6
mikachou <chou1005.tw@gmail.com>
2023-07-31 16:37:09 +0800

    adjust className

--- src/components/Main/Cart/CartRenderItem.jsx
+++ src/components/Main/Cart/CartRenderItem.jsx
@@ -11,17 +11,21 @@ const CartRenderItem = ({ handleQtyChange, img, name, price, quantity }) => {
           <h5>單價: NTD {price}</h5>
         </div>
         <div className="addItem">
-          <img
-            src="/icons/minus.svg"
-            alt=""
-            onClick={() => handleQtyChange(quantity - 1)}
-          />
-          <p>{quantity}</p>
-          <img
-            src="/icons/plus.svg"
-            alt=""
-            onClick={() => handleQtyChange(quantity + 1)}
-          />
+          <div className="btnItems">
+            <img
+              className="minusIcon"
+              src="/icons/minus.svg"
+              alt=""
+              onClick={() => handleQtyChange(quantity - 1)}
+            />
+            <p className="qtyRender">{quantity}</p>
+            <img
+              className="plusIcon"
+              src="/icons/plus.svg"
+              alt=""
+              onClick={() => handleQtyChange(quantity + 1)}
+            />
+          </div>
           <h5 className="currentPrice">
             {getFormattedPrice(price * quantity)}
           </h5>
