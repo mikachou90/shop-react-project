e2ab01bf835f5cff6e3d1ca05c5e5d4985466d7d
mikachou <chou1005.tw@gmail.com>
2023-08-04 16:18:52 +0800

    move cartRenderItem to Cart.jsx

--- src/Components/Main/Cart/Cart.jsx
+++ src/Components/Main/Cart/Cart.jsx
@@ -1,9 +1,40 @@
 import "./Cart.css";
-import CartRenderItem from "./CartRenderItem";
 import { useContext } from "react";
 import { getFormattedPrice } from "Utils/pricing";
 import CartContext from "./CartContext";
 import FormContext from "../Form/FormContext";
+
+const CartRenderItem = ({ product, handleQtyChange }) => {
+  return (
+    <div className="item">
+      <img className="itemPic" src={product.img} alt={product.name} />
+      <div className="itemRright">
+        <div className="itemInfo">
+          <h5>{product.name}</h5>
+          <h5>單價: NTD {product.price}</h5>
+        </div>
+        <div className="addItem">
+          <div className="btnItems">
+            <img
+              src="/icons/minus.svg"
+              alt=""
+              onClick={() => handleQtyChange(product.quantity - 1)}
+            />
+            <p className="qtyRender">{product.quantity}</p>
+            <img
+              src="/icons/plus.svg"
+              alt=""
+              onClick={() => handleQtyChange(product.quantity + 1)}
+            />
+          </div>
+          <h5 className="currentPrice">
+            {getFormattedPrice(product.price * product.quantity)}
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function Cart() {
   const { currentItems, setCurrentItems, totalValuesItems } =
