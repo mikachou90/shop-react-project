6162be87f63dc715fd2ab14e996d5b4190d2346f
mikachou <chou1005.tw@gmail.com>
2023-08-04 14:52:48 +0800

    delete useState in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../src/App.css";
 import Header from "Components/Header/Header";
 import Footer from "Components/Footer/Footer";
